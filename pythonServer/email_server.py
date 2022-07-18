640c284da5079cf98f53f8633199c5c16d541983
Grant Mitchell <soundbreakr1@gmail.com>
2022-07-17 23:46:47 -0700

    added new import statement to fix collections outdated error

--- pythonServer/email_server.py
+++ pythonServer/email_server.py
@@ -13,6 +13,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from string_interpolate import interpolate
+
 # gv = open('./credentials/global_variables.json')
 # jsonstring = gv.read()
 # jsonobj = json.loads(jsonstring)
--- pythonServer/py_test/email_server_test.py
+++ pythonServer/py_test/email_server_test.py
@@ -1,4 +1,10 @@
 # content of test_class.py
+import collections
+try:
+    collectionsAbc = collections.abc
+except AttributeError:
+    collectionsAbc = collections
+
 class TestClass:
     def test_one(self):
         x = "this"
