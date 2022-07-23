1a650d3bbe4bab1241bdbe031363b136495ad9e5
Grant Mitchell <soundbreakr1@gmail.com>
2022-07-23 13:24:00 -0700

    changed yaml file to on pull requests and updated resume to be scalablle

--- nodeServer/src/Pages/About/about.js
+++ nodeServer/src/Pages/About/about.js
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import {React,useState,useEffect} from 'react';
 import './about.scss';
 import BackgroundTop from './BackgroundTop';
 import ProfileImage from './ProfileImage'
@@ -7,6 +7,43 @@ import '../../global.scss';
 import Frame from './frame'
 
 const About = function (props){
+  var path = "/images/Resume.pdf"
+  var ww= window.innerWidth
+
+  const [url, setURL] = useState(path+"#zoom=100")
+
+    var checkWidth = (window_width)=>{
+      if (window_width <= 550){
+        setURL(path+"#zoom=50")
+      }
+      if (window_width <= 700 && window_width > 550){
+        setURL(path+"#zoom=60")
+      }
+      if ( window_width > 700 && window_width <= 900){
+        setURL(path+"#zoom=70")
+      }
+      if (window_width > 900 && window_width < 1200){
+        setURL(path+"#zoom=80")
+      }
+      if (window_width > 1200){
+        setURL(path+"#zoom=100")
+      }
+    }
+
+    useEffect(() => {
+
+      checkWidth(ww)
+
+       var handler = (e) =>{
+        var window_width = e.target.innerWidth
+        checkWidth(window_width)
+      }
+      console.log('path',path)
+      window.addEventListener('resize',handler);
+      console.log('path',path)
+
+      return () => window.removeEventListener('resize', handler);
+    });
 
 return (
   <div className="about">
@@ -14,7 +51,7 @@ return (
     <div className="profile_info center">
       <ProfileImage></ProfileImage>
       <ProfileDescription></ProfileDescription>
-  <Frame url='/images/resume.pdf' text={'My resume'}></Frame>
+  <Frame key={url}  url={url} text={'My resume'}></Frame>
     </div>
   </div>);
 
